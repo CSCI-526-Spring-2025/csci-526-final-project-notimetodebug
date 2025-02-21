45e3fcc03a1080a8f7475639b66d23c15df5b5cd
gbccccc <gbccccc233@gmail.com>
2025-02-20 22:21:16 -0800

    player gun bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float velocity = 20;
+
+    private Rigidbody2D rb;
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.FromToRotation(Vector3.right, rb.velocity);
+    }
+
+    public void Fire(Vector3 direction)
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+        rb.velocity = direction * velocity;
+        transform.rotation = Quaternion.FromToRotation(Vector3.right, rb.velocity);
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public Vector3 fireDirection;
+
+    public Gun gun;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        fireDirection = (mousePos - transform.position).normalized;
+        gun.SetDirection(fireDirection);
+        // Debug.DrawLine(transform.position, transform.position + shootDirection, Color.red);
+
+        if (Input.GetMouseButton(0) && gun is not null)
+        {
+            Vector3 recoilForce = gun.Fire(fireDirection);
+            rb.AddForce(recoilForce, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && gun is not null)
+        {
+            gun.Reload();
+        }
+    }
+}
