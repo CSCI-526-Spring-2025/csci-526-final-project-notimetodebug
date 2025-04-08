6ebd3a07f2b4aece18e8f130db8d2dbf6af21551
unknown <manthenasimha@gmail.com>
2025-04-08 01:55:16 -0700

    Added shield enemy

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Shield : MonoBehaviour, IBulletIteractable
+{
+    public int shieldHealth = 100;
+
+    public virtual void OnBulletCollision(Bullet bullet)
+    {
+        bullet.OnAbsorbed();  
+
+        shieldHealth -= bullet.damage;
+
+        if (shieldHealth <= 0)
+        {
+            DestroyShield();
+        }
+    }
+
+    private void DestroyShield()
+    {
+        Destroy(gameObject);  
+    }
+}
