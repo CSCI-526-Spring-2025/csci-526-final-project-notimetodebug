6c4c5aa3987bec3b0a046270000fbd34cfdaad66
Sreekant Nair <92329674+SREEKANT10@users.noreply.github.com>
2025-04-08 10:43:31 -0700

    updated switching of the collectible

--- Assets/Scripts/CollectibleGun.cs
+++ Assets/Scripts/CollectibleGun.cs
@@ -2,93 +2,10 @@ using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.Serialization;
 
-
-// public class CollectibleGun : MonoBehaviour
-// {
-//     [Header("Gun Properties")]
-//     public Gun gunPrefab; 
-    
-//     [Header("Floating Animation")]
-//     public float bobHeight = 0.5f;
-//     public float bobSpeed = 3.0f;
-//     public float rotationSpeed = 10.0f;
-    
-//     private Vector3 startPosition;
-//     private bool isCollected = false;
-    
-//     void Start()
-//     {
-//         startPosition = transform.position;
-//     }
-    
-//     void Update()
-//     {
-
-//         if (!isCollected)
-//         {
-//             // Bobbing up and down
-//             float newY = startPosition.y + Mathf.Sin(Time.time * bobSpeed) * bobHeight;
-//             transform.position = new Vector3(transform.position.x, newY, transform.position.z);
-            
-//             transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
-//         }
-//     }
-    
-//     void OnTriggerEnter2D(Collider2D other)
-//     {
-
-//         if (isCollected)
-//             return;
-        
-//         Player player = other.GetComponent<Player>();
-        
-//         if (player != null)
-//         {
-//             isCollected = true;
-            
-//             // Instantiate the gun prefab as a child of the player
-//             Gun newGun = Instantiate(gunPrefab, player.transform);
-            
-//             if (newGun.transform != null)
-//             {
-//                 newGun.transform.localPosition = Vector3.zero;
-//                 newGun.transform.localRotation = Quaternion.identity;
-//             }
-            
-//             player.PickUpGun(newGun);
-            
-//             Renderer renderer = GetComponent<Renderer>();
-//             if (renderer != null)
-//             {
-//                 renderer.enabled = false;
-//             }
-            
-//             // Disable any child renderers
-//             Renderer[] childRenderers = GetComponentsInChildren<Renderer>();
-//             foreach (Renderer r in childRenderers)
-//             {
-//                 r.enabled = false;
-//             }
-            
-//             // Disable the collider
-//             Collider2D collider = GetComponent<Collider2D>();
-//             if (collider != null)
-//             {
-//                 collider.enabled = false;
-//             }
-
-//             TelemetryManager.Log(TelemetryManager.EventName.COLLECTIBLE_PICKED_UP, $"Gun: {gunPrefab.name}");
-
-//             // Destroy the collectible
-//             Destroy(gameObject, 0.1f);
-//         }
-//     }
-// }
-
 public class CollectibleGun : MonoBehaviour
 {
     [Header("Gun Properties")]
-    public Gun gunPrefab;
+    public Gun gunPrefab; 
     
     [Header("Floating Animation")]
     public float bobHeight = 0.5f;
@@ -97,9 +14,6 @@ public class CollectibleGun : MonoBehaviour
     
     private Vector3 startPosition;
     private bool isCollected = false;
-    
-    // For dropped guns, optionally store additional properties 
-    // private int currentAmmo;
     
     void Start()
     {
@@ -129,7 +43,40 @@ public class CollectibleGun : MonoBehaviour
         {
             isCollected = true;
             
-            // Instantiate the gun prefab as a child of the player
+            // Get the position of this collectible 
+            Vector3 collectiblePosition = transform.position;
+            
+            
+            if (player.guns.Count > 1)
+            {
+                Gun oldGun = player.guns[1];
+                
+                bool wasEquipped = (player.currentGunIndex == 1);
+                
+                
+                if (wasEquipped)
+                {
+                    oldGun.OnUnequipped();
+                }
+                
+                
+                if (GunManager.Instance != null)
+                {
+                    GunManager.Instance.CreateCollectibleFromGun(oldGun, collectiblePosition);
+                }
+              
+                oldGun.Destroy();
+                player.guns.RemoveAt(1);
+                
+        
+                if (wasEquipped)
+                {
+                    player.currentGunIndex = 0;
+                    player.guns[0].OnEquipped();
+                }
+            }
+            
+   
             Gun newGun = Instantiate(gunPrefab, player.transform);
             
             if (newGun.transform != null)
@@ -138,50 +85,39 @@ public class CollectibleGun : MonoBehaviour
                 newGun.transform.localRotation = Quaternion.identity;
             }
             
-            // Transfer any special properties to the gun if this was a dropped gun
-            // newGun.currentAmmo = currentAmmo;
-            
+    
             player.PickUpGun(newGun);
             
-            // Hide the collectible
+  
             SetVisible(false);
             
             TelemetryManager.Log(TelemetryManager.EventName.COLLECTIBLE_PICKED_UP, $"Gun: {gunPrefab.name}");
 
-            // Destroy the collectible
+         
             Destroy(gameObject, 0.1f);
         }
     }
     
     private void SetVisible(bool visible)
     {
-        // Hide or show all renderers
+
         Renderer renderer = GetComponent<Renderer>();
         if (renderer != null)
         {
             renderer.enabled = visible;
         }
         
-        // Disable any child renderers
+       
         Renderer[] childRenderers = GetComponentsInChildren<Renderer>();
         foreach (Renderer r in childRenderers)
         {
             r.enabled = visible;
         }
-        
-        // Disable the collider
+     
         Collider2D collider = GetComponent<Collider2D>();
         if (collider != null)
         {
             collider.enabled = visible;
         }
-    }
-    
-    // Method to create a collectible from an existing gun (for gun manager)
-    public static CollectibleGun CreateFromGun(Gun gun, Vector3 position)
-    {
-        // This would be called by a GunManager to create collectibles
-        // from guns that have been dropped
-        return null; // Implementation depends on your game structure
     }
 }
--- Assets/Scripts/GunManager.cs
+++ Assets/Scripts/GunManager.cs
@@ -8,14 +8,15 @@ public class GunManager : MonoBehaviour
     [System.Serializable]
     public class GunMapping
     {
-        public string gunName;
-        public Gun gunPrefab;
-        public CollectibleGun collectiblePrefab;
+        public string gunName;  
+        public Gun gunPrefab;   
+        public CollectibleGun collectiblePrefab;  
     }
     
     [SerializeField] private List<GunMapping> gunMappings = new List<GunMapping>();
+    [SerializeField] private CollectibleGun defaultCollectiblePrefab; // Fallback collectible
     
-    private Dictionary<string, CollectibleGun> gunToCollectibleMap = new Dictionary<string, CollectibleGun>();
+    private Dictionary<string, GunMapping> gunToMappingMap = new Dictionary<string, GunMapping>();
     
     private void Awake()
     {
@@ -27,34 +28,44 @@ public class GunManager : MonoBehaviour
         else
         {
             Destroy(gameObject);
+            return;
         }
         
         // Build the lookup map
         foreach (var mapping in gunMappings)
         {
-            gunToCollectibleMap[mapping.gunName] = mapping.collectiblePrefab;
+            gunToMappingMap[mapping.gunName] = mapping;
         }
     }
     
-    // public CollectibleGun GetCollectiblePrefabForGunType(Gun gun)
-    // {
-    //     if (gunToCollectibleMap.TryGetValue(gun.name.Replace("(Clone)", ""), out CollectibleGun collectible))
-    //     {
-    //         return collectible;
-    //     }
-        
-    //     Debug.LogWarning($"No collectible prefab found for gun: {gun.name}");
-    //     return null;
-    // }
-    public CollectibleGun GetCollectiblePrefabForGunType(Gun gun)
+    public CollectibleGun CreateCollectibleFromGun(Gun gun, Vector3 position)
     {
-        // Use the gun name without the "(Clone)" suffix that Unity adds to instantiated objects
-        if (gunToCollectibleMap.TryGetValue(gun.name.Replace("(Clone)", ""), out CollectibleGun collectible))
+        
+        string gunBaseName = gun.name.Replace("(Clone)", "");
+        
+        if (gunToMappingMap.TryGetValue(gunBaseName, out GunMapping mapping))
         {
-            return collectible;
+          
+            CollectibleGun newCollectible = Instantiate(mapping.collectiblePrefab, position, Quaternion.identity);
+            newCollectible.gunPrefab = mapping.gunPrefab; 
+            return newCollectible;
         }
         
-        Debug.LogWarning($"No collectible prefab found for gun: {gun.name}");
+        Debug.LogWarning($"No mapping found for gun: {gunBaseName}, using default collectible");
+        if (defaultCollectiblePrefab != null)
+        {
+            return Instantiate(defaultCollectiblePrefab, position, Quaternion.identity);
+        }
+        
+        return null;
+    }
+    
+    public Gun GetGunPrefabByName(string gunName)
+    {
+        if (gunToMappingMap.TryGetValue(gunName, out GunMapping mapping))
+        {
+            return mapping.gunPrefab;
+        }
         return null;
     }
 }
